58e9d85ef38a0e6f4733bb0ccb8762075efe38ab
Harsha Vardhan Koneru <harshavardhankoneru@Harshas-MacBook-Pro.local>
2021-10-14 10:21:20 -0700

    React App skelton added

---
+++ app/src/components/Components_Template.js
@@ -0,0 +1,13 @@
+/**
+ * Components Folder should be used to construct reusable components 
+ */
+
+// Example Ideas that can be included in this namespace
+/**
+ * Pagination
+ * Button
+ * Icon
+ * Action Menu
+ * Header
+ * any logic that can be reusable
+ */
---
+++ app/src/views/home/main.js
@@ -0,0 +1,20 @@
+/**
+ * All the core components come here
+ * These components are parent UI elements
+ */
+
+// Like
+/**
+ * Home Page
+ * Contextual Playground for POC etc.
+ */
+
+
+function Main(props) {
+    // some implementation
+    return (
+        <div id="root">Sample Template</div>
+    )
+}
+
+export default Main
