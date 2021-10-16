9c320befe56549ac8cff178204fc17d4fb80bfa4
Harsha Vardhan Koneru <harshavardhankoneru@Harshas-MacBook-Pro.local>
2021-10-16 00:34:19 -0700

    Added Main page

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,8 +2,48 @@
  * We can perform user level authentications here later on using contextual apis (custom)
  * We can route to different components defined in pages directory from here
  */
-export default function Home() {
+import React from "react";
+import logo from "../public/assets/logo.jpg";
+
+export default function Main() {
+  console.log(logo);
   return (
-    <div></div>
-  )
+    <div className="container main-container">
+      <div className="row">
+        <div className="col-md-5 main_content">
+          <div className="content">
+            <img src={logo.src} />
+            <p>
+              Content Content Content Content Content Content Content Content
+              Content Content Content Content Content Content Content Content
+              Content Content Content Content Content Content Content Content
+              Content Content Content Content Content Content Content Content
+              Content Content Content Content Content Content Content Content
+              Content Content Content Content Content Content Content Content
+              Content
+            </p>
+            <a class="btn btn-primary" href="#" role="button">
+              Personal
+            </a>
+            <br />
+            <br />
+            <a class="btn btn-primary" href="#" role="button">
+              Organisation
+            </a>
+          </div>
+        </div>
+        <div className="col-md-7 fit-image main_img">
+          <div
+            className="offset-md-1 main_img_text"
+            style={{ position: "absolute", bottom: "50px" }}
+          >
+            <p className="site-title">
+              Welcome To Clinical Risk Authentication as a Service
+            </p>
+            <p className="sub-site-title">Analytics Portal</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
