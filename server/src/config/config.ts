bd3c2429f3728784db65d27a7ec1878dd8bcc590
Harsha Vardhan Koneru <harshavardhankoneru@Harshas-MacBook-Pro.local>
2021-11-01 21:33:14 -0700

    Create User API added

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,3 +1,6 @@
 export default {
-    PORT: process.env.PORT || '8080'
+    PORT: process.env.PORT || '8080',
+    AWS_ACCESS_KEY: 'AKIATVZTOASY7MNCH2PD',
+    AWS_SECRET_KEY: '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',
+    AWS_REGION: 'us-east-2'
   };
