5eecaef96ae2479735ee73602b55f5624a3cd6b0
Harsha Vardhan Koneru <harshavardhankoneru@Harshas-MacBook-Pro.local>
2021-11-02 16:53:34 -0700

    Modified both APIs, Changed AUTH schema

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
     PORT: process.env.PORT || '8080',
-    AWS_ACCESS_KEY: 'AKIATVZTOASY7MNCH2PD',
-    AWS_SECRET_KEY: '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',
+    AWS_ACCESS_KEY: 'AKIATVZTOASYXQXG2UAC',
+    AWS_SECRET_KEY: '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',
     AWS_REGION: 'us-east-2'
   };
